11f01d29a1b8061e145abebabe41b6bf6f3609c5
Chizhov Vitaliy <chizhov.vitaliy@inbox.ru>
2025-04-21 20:34:44 +0300

    AlexLion class

---
+++ src/main/java/com/example/AlexLion.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.util.List;
+
+public class AlexLion extends Lion {
+
+    public AlexLion(Feline feline) throws Exception {
+        super("Самец", feline);
+
+    }
+
+    public String getPlaceOfLiving() {
+        return "Нью-Йоркский зоопарк";
+    }
+
+    public List<String> getFriends() {
+        return List.of("Марти", "Глория", "Мелман");
+    }
+
+    @Override
+    public int getKittens() {
+        return 0;
+    }
+
+}
---
+++ src/test/java/LionParadmeterizedTest.java
@@ -0,0 +1,2 @@
+public class LionParadmeterizedTest {
+}
